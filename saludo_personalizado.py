4f44e8aa0a0316f05aaaa863b574db2665a86ff5
Valeria <05valarist@gmail.com>
2024-11-19 09:46:30 -0500

    arreglo saludo_personalizado

--- saludo_personalizado.py
+++ saludo_personalizado.py
@@ -1,7 +1,16 @@
 import json
 
+
 def lambda_handler(event, context):
+    nombre = event.get('queryStringParameters', {}).get('nombre')
+
+    if not nombre:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'Falta el parámetro "nombre"'})
+        }
+
     return {
-        "statusCode": 200,
-        "body": json.dumps({"message": "Hola, bienvenido al sistema!"})
+        'statusCode': 200,
+        'body': json.dumps({'message': f'Hola, {nombre}!'})
     }
